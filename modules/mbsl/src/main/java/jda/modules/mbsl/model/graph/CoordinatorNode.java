28e4b39673fa887eafd3e72d3529e5039f9dab5a
Duc Minh Le <lemduc@gmail.com>
2023-02-24 12:07:20 +0700

    CoordinatorNode: added explanation

--- modules/mbsl/src/main/java/jda/modules/mbsl/model/graph/CoordinatorNode.java
+++ modules/mbsl/src/main/java/jda/modules/mbsl/model/graph/CoordinatorNode.java
@@ -7,7 +7,11 @@ import jda.mosa.module.ModuleService;
  * @overview 
  *  Coordinator Node is a Node that does not pass its data out to the outgoing edges. Instead, it passes the original input data out.
  *  
- *  <br>Coordinator nodes are typically used to capture coordinated tasks. 
+ *  <br>Coordinator node is used to represent a task group. The coordinator's UI serves as the container of those of the member tasks, so that user can have a whole picture of the group. The member tasks themselves interact with each other to perform the group's logic.
+ *  
+ *  <p>Example: Fulfill Order is a coordinator node for the following two tasks: Order Update Order Delivery. "Order Update" basic updates the Order status to "Fulfilled" after the order has actually been fulfilled by the warehouse. Once this has been done, "Order Delivery" is performed to deliver the ordered products to the customer and to collect payment.
+ *  
+ *   <p>In the above use case, Fulfill Order simply coordinates the other two tasks, ensuring that Order Update is performed first then Delivery. It does not contribute any data to this flow. However, it enables user to observe and perform the task flow on the UI.
  *  
  * @author Duc Minh Le (ducmle)
  *
